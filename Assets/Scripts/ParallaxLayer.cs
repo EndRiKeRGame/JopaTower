a6611be0851315e302f45bb837a95b295465ab09
EndRiKeR <solomenckov.petya@ya.ru>
2026-08-13 16:17:45 +0300

    save progress

---
+++ Assets/Scripts/ParallaxLayer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+using UnityEngine;
+
+public class ParallaxLayer : MonoBehaviour
+{
+    [SerializeField] private float parallaxFactor = 0.5f; // 0 = статичный фон, 1 = движется как камера
+    private Transform cameraTransform;
+    private Vector3 previousCameraPosition;
+
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        previousCameraPosition = cameraTransform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 delta = cameraTransform.position - previousCameraPosition;
+        transform.position += new Vector3(delta.x * parallaxFactor, delta.y * parallaxFactor, 0);
+        previousCameraPosition = cameraTransform.position;
+    }
+}
