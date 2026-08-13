04aaba491dc3f8f7b02a27373b93305b0389715a
EndRiKeR <solomenckov.petya@ya.ru>
2026-08-13 12:02:41 +0300

    add sorting layers

--- Assets/Scripts/Ui/MainMenuView.cs
+++ Assets/Scripts/Ui/MainMenuView.cs
@@ -16,15 +16,42 @@ namespace DefaultNamespace.Ui
         [SerializeField]
         private CanvasGroup _canvasGroup;
         
+        [SerializeField]
+        private ScrollRect _scrollRect;
+
+        [SerializeField]
+        private Image _image;
+        
         [Header("Animation values")]
         [SerializeField]
         private float _animationDuration = 0.2f;
+        
+        [SerializeField]
+        private float _colorChangeAnimationDuration = 10f;
+        
+        [SerializeField]
+        private AnimationCurve _animCurve;
+
+        private Scrollbar _scrollBar;
 
         private void Awake()
         {
             _canvasGroup.blocksRaycasts = true;
             _canvasGroup.alpha = 1;
+            _scrollBar = _scrollRect.horizontalScrollbar;
+            Tween.Custom(0f, 1f, new TweenSettings(600f, Ease.Default, cycleMode: CycleMode.Rewind, cycles: -1), f => _scrollBar.value = f);
             
+            Sequence seq = Sequence.Create();
+            
+            var firstState = Tween.Color(_image, new TweenSettings<Color>(new Color(1f, 1f, 0f, 0.8f), _colorChangeAnimationDuration));
+            var secondState = Tween.Color(_image, new TweenSettings<Color>(new Color(1f, 0f, 1f, 0.8f), _colorChangeAnimationDuration));
+            var thirdState = Tween.Color(_image, new TweenSettings<Color>(new Color(0f, 1f, 1f, 0.8f), _colorChangeAnimationDuration));
+            
+            seq.Chain(firstState);
+            seq.Chain(secondState);
+            seq.Chain(thirdState);
+            seq.SetRemainingCycles(-1);
+
             _startButton.onClick.AddListener(StartGame);
         }
 
